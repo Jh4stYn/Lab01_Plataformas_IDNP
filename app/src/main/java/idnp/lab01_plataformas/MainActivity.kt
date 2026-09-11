6f9489b974b416b3d2a9200a4a5de3b6c347f56f
Jh4stYn <jpayehuancar@unsa.edu.pe>
2026-09-10 22:33:32 -0500

    feat: Configuración inicial y prueba de recomposición de textos

---
+++ app/src/main/java/idnp/lab01_plataformas/MainActivity.kt
@@ -0,0 +1,48 @@
+package idnp.lab01_plataformas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import idnp.lab01_plataformas.ui.theme.Lab01_PlataformasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab01_PlataformasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "¡Hola, $name! Laboratorio 01-IDNP")
+        Text(text = "Comprobando la recomposición en Compose")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab01_PlataformasTheme {
+        Greeting("Android")
+    }
+}
